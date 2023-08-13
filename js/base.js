564d1ce17edba430116cd38cd10248e09649db55
PaiktraNom <paiktranom@gmail.com>
2023-08-13 19:01:46 -0400

    testing nav

--- js/base.js
+++ js/base.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
     $("#includedContent").load("base.html"); 
-
-
-            // Adding click event on id div-1
-            // if it clicked then anonymous
-            // function will be called
-            $('#home').click(function () {
- 
-                // Load the external html
-                // here this refers to
-                // current selector
-                $(this).load('index.html');
-            });
-            // Same as above
-            $('#about').click(function () {
-                $(this).load('about.html');
-            });
-
-            $('#contact').click(function () {
-                $(this).load('contact.html');
-            });
   });
+
+  function myFunc(){
+    window.open("https://www.google.com");
+  }
