6d645f1f99cb2e2ea6009640288a7f740588a418
PaiktraNom <paiktranom@gmail.com>
2023-08-13 18:28:18 -0400

    Created basic navigation

--- js/base.js
+++ js/base.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
     $("#includedContent").load("base.html"); 
+
+
+            // Adding click event on id div-1
+            // if it clicked then anonymous
+            // function will be called
+            $('#home').click(function () {
+ 
+                // Load the external html
+                // here this refers to
+                // current selector
+                $(this).load('index.html');
+            });
+            // Same as above
+            $('#about').click(function () {
+                $(this).load('about.html');
+            });
+
+            $('#contact').click(function () {
+                $(this).load('contact.html');
+            });
   });
