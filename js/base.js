2a6b1478f18d90e7e9d75e5066dda2994bbf3523
PaiktraNom <paiktranom@gmail.com>
2023-08-13 22:43:05 -0400

    Added contact me functionality

--- js/base.js
+++ js/base.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-    $(document).ready(function(){
-        $("button").on("click", function(e) {
-            var input = $(this).val();
-            // prevent page submit
-            e.preventDefault(); 
-            $("button").removeClass("active");
-            $(this).toggleClass("active");
-            $(".page").load(input);
-        });
+    $('#contact').click(function (event) {
+        var email = 'paiktranom@gmail.com';
+        var subject = 'Contact Paiktra Nom';
+        var emailBody = 'Hi Paiktra,\n';
+        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+      });
+
+    $(".nav").on("click", function(e) {
+        var input = $(this).val();
+        // prevent page submit
+        e.preventDefault(); 
+        $("button").removeClass("active");
+        $(this).toggleClass("active");
+        $(".page").load(input);
     });
 });
