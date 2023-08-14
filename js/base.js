bba5aa02c23beeb189de0f46e57cf800ca791953
PaiktraNom <paiktranom@gmail.com>
2023-08-14 10:41:56 -0400

    Removed flashing tag bug

--- js/base.js
+++ js/base.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    $(".page").load("templates/home.html");
-
-    $('#contact').click(function (event) {
-        var email = 'paiktranom@gmail.com';
-        var subject = 'Contact Paiktra Nom';
-        var emailBody = 'Hi Paiktra,\n';
-        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
-      });
-
-    $(".nav").on("click", function(e) {
-        var input = $(this).val();
-        // prevent page submit
-        e.preventDefault(); 
+  $(".content").load("templates/home.html");
+});
+$(".nav-item button").on("click", function(e) {
+    if ($(this).attr("class") === "nav"){
+        var input = $(this).val(); 
         $("button").removeClass("active");
         $(this).toggleClass("active");
+        // prevent page submit
+        e.preventDefault();
         $(".page").load(input);
-    });
+    }
 });
+
+$('#contact').click(function (event) {
+    var email = 'paiktranom@gmail.com';
+    var subject = 'Contact Paiktra Nom';
+    var emailBody = 'Hi Paiktra,\n';
+    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+  });
