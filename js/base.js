467f8628d16c0b23b728ef682ea76b403009369b
PaiktraNom <paiktranom@gmail.com>
2023-08-20 13:32:08 -0400

    Made the about section pretty

--- js/base.js
+++ js/base.js
@@ -21,21 +21,18 @@ $( document ).ready(function() {
         .then(data => {
             document.querySelector(".end").innerHTML = data;
         });
-    
-    $('#contact').on("click", function (e) {
-        e.stopImmediatePropagation();
-        document.getElementById('scroll-to-end').scrollIntoView({block: "start", behavior: "smooth"});
-        // prevent page submit
-        e.preventDefault();
-    });
-    
-    $('#email').on("click", function (e) {
-        var email = 'paiktranom@gmail.com';
-        var subject = 'Contact Paiktra Nom';
-        var emailBody = 'Hi Paiktra,\n';
-        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
-        // prevent page submit
-        e.preventDefault();
-    });
 });
+
+function scrollToEnd() {
+    document.getElementById('scroll-to-end').scrollIntoView({ block: "start", behavior: "smooth" });
+    return false;
+}
+
+function email(){
+    var email = 'paiktranom@gmail.com';
+    var subject = 'Contact Paiktra Nom';
+    var emailBody = 'Hi Paiktra,\n';
+    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+    return false;
+}
 
